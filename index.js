3a6af49beb12d16cb418a0daf2c8cce04280b2bb
yusalz <84872458+yusalz@users.noreply.github.com>
2021-12-21 22:06:46 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import axios from 'axios';
+
+const PORT = process.env.PORT || 4321;
+
+const app = express();
+app.get('/wordpress/', async (req, res, next) => { 
+    const content = req.query.content; 
+    const resp = await axios.post(
+        'https://wordpress.kodaktor.ru/wp-json/jwt-auth/v1/token', 
+        { username: 'gossjsstudent2017', password: '|||123|||456' }, 
+    ); 
+    const token = resp.data.token;
+    
+    const wpRes = await axios.post(
+        'https://wordpress.kodaktor.ru/wp-json/wp/v2/posts', 
+        { content, title: 'liubov97', status: 'publish' },
+        { 
+            headers: { 
+                Authorization: `Bearer ${token}`, 
+            }, 
+        }, 
+    ); 
+    
+    res.send(wpRes.data.id + '');
+    })
+
+    .all("/login", (r) => r.res.send("itmo309692"))
+
+    .listen(PORT, ()=>{
+        console.log('server is listening');
+    });
+
